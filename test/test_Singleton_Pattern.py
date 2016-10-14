52e0ec7b6f09eda0d1de0eb31995b2495a7ddcb3
IvanaXu <1440420407@qq.com>
2016-10-14 19:33:54 +0800

    Create test_Singleton_Pattern.py

---
+++ test/test_Singleton_Pattern.py
@@ -0,0 +1,23 @@
+# -*- coding:utf-8 -*-
+# @auth ivan
+# @time 2016-10-14 17:16:26
+# @goal test Singleton Pattern
+
+
+class SingleObject:
+    def __init__(self):
+        self.instance = None
+
+    def getInstance(self):
+        self.instance = SingleObject()
+        return self.instance
+
+    def showMessage(self):
+        print 'Hello World'
+
+
+class SingletonPatternDemo:
+    def __init__(self):
+        object = SingleObject().getInstance()
+        object.showMessage()
+SingletonPatternDemo()
