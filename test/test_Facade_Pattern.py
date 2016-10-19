69f706ed3e4611e616498e463530784c7e354fa5
IvanaXu <1440420407@qq.com>
2016-10-19 21:57:23 +0800

    Create test_Facade_Pattern.py

---
+++ test/test_Facade_Pattern.py
@@ -0,0 +1,55 @@
+# -*- coding:utf-8 -*-
+# @auth ivan
+# @time 2016-10-19 21:43:58 
+# @goal test for Facade Pattern
+
+
+class Shape:
+    def __init__(self):
+        return
+
+    def draw(self):
+        return
+
+
+class Rectangle(Shape):
+    def draw(self):
+        print "Rectangle::draw()"
+
+
+class Square(Shape):
+    def draw(self):
+        print "Square::draw()"
+
+
+class Circle(Shape):
+    def draw(self):
+        print "Circle::draw()"
+# Rectangle Square Circle
+
+
+class ShapeMaker:
+    def __init__(self):
+        self.circle = Circle()
+        self.rectangle = Rectangle()
+        self.square = Square()
+
+    def drawCircle(self):
+        self.circle.draw()
+
+    def drawRectangle(self):
+        self.rectangle.draw()
+
+    def drawSquare(self):
+        self.square.draw()
+
+
+class FacadePatternDemo:
+    def run(self):
+        shapeMaker = ShapeMaker()
+        shapeMaker.drawCircle()
+        shapeMaker.drawRectangle()
+        shapeMaker.drawSquare()
+F = FacadePatternDemo()
+F.run()
+
