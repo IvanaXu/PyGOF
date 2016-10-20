9e0322f2d6a53d4e6ca2a7c68ae3450ff0d631d9
IvanaXu <1440420407@qq.com>
2016-10-20 21:14:25 +0800

    Create test_Command_Pattern.py

---
+++ test/Behavioral_Patterns/test_Command_Pattern.py
@@ -0,0 +1,71 @@
+# -*-coding:utf-8-*-
+# @auth ivan
+# @time 2016-10-20 20:53:16
+# @goal test for Command Pattern
+
+
+class Order:
+    def __init__(self):
+        return
+
+    def execute(self):
+        return
+
+
+class Stock:
+    def __init__(self):
+        self.name = 'ABC'
+        self.quantity = 10
+
+    def buy(self):
+        print "Stock [ Name: " + self.name + ", Quantity: " + str(self.quantity) + " ] bought"
+
+    def sell(self):
+        print "Stock [ Name: " + self.name + ", Quantity: " + str(self.quantity) + " ] sold"
+
+
+class BuyStock(Order):
+    def __init__(self, abcStock):
+        Order.__init__(self)
+        self.abcStock = abcStock
+
+    def execute(self):
+        self.abcStock.buy()
+
+
+class SellStock(Order):
+    def __init__(self, abcStock):
+        Order.__init__(self)
+        self.abcStock = abcStock
+
+    def execute(self):
+        self.abcStock.sell()
+
+
+class Broker:
+    def __init__(self):
+        self.orderList = []
+
+    def takeOrder(self, order):
+        self.orderList.append(order)
+
+    def placeOrders(self):
+        for order in self.orderList:
+            order.execute()
+        self.orderList = []
+
+
+class CommandPatternDemo:
+    def run(self):
+        abcStock = Stock()
+        buyStockOrder = BuyStock(abcStock)
+        sellStockOrder = SellStock(abcStock)
+
+        broker = Broker()
+        broker.takeOrder(buyStockOrder)
+        broker.takeOrder(sellStockOrder)
+
+        broker.placeOrders()
+C = CommandPatternDemo()
+C.run()
+
