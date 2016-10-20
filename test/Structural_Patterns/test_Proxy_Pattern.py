91c3bf6aa8fc106bbe922cc8b869923e6f60b89a
IvanaXu <1440420407@qq.com>
2016-10-20 20:47:36 +0800

    Create test_Proxy_Pattern.py

---
+++ test/Structural_Patterns/test_Proxy_Pattern.py
@@ -0,0 +1,49 @@
+# -*-coding:utf-8-*-
+# @auth ivan
+# @time 2016-10-20 19:11:22
+# @goal test for Proxy Pattern
+
+
+class Image:
+    def __init__(self):
+        return
+
+    def Image(self):
+        return
+
+
+class RealImage(Image):
+    def __init__(self, filename):
+        Image.__init__(self)
+        self.filename = filename
+        self.loadFromDisk()
+
+    def display(self):
+        print "Displaying " + self.filename
+
+    def loadFromDisk(self):
+        print "Loading " + self.filename
+# R = RealImage('file')
+
+
+class ProxyImage(Image):
+    def __init__(self, filename):
+        Image.__init__(self)
+        self.filename = filename
+        self.realImage = None
+
+    def display(self):
+        if not self.realImage:
+            self.realImage = RealImage(self.filename)
+        self.realImage.display()
+
+
+class ProxyPatternDemo:
+    def run(self):
+        image = ProxyImage('test_10mb.jpg')
+        image.display()
+        print ' '
+        image.display()
+P = ProxyPatternDemo()
+P.run()
+
