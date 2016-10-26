c65b01cadb06a74fb4dbddcc8d174116a239b58c
IvanaXu <1440420407@qq.com>
2016-10-26 11:06:33 +0800

    Create test_Template_Pattern.py

---
+++ test/Behavioral_Patterns/test_Template_Pattern.py
@@ -0,0 +1,66 @@
+# -*-coding:utf-8-*-
+# @auth ivan
+# @time 2016-10-26 10:56:29
+# @goal test for Template Pattern
+
+
+class Game:
+    def __init__(self):
+        return
+
+    def initialize(self):
+        return
+
+    def startPlay(self):
+        return
+
+    def endPlay(self):
+        return
+
+    def play(self):
+        self.initialize()
+
+        self.startPlay()
+
+        self.endPlay()
+
+
+class Cricket(Game):
+    def __init__(self):
+        Game.__init__(self)
+
+    def initialize(self):
+        print "Cricket Game Initialized! Start playing."
+
+    def startPlay(self):
+        print "Cricket Game Started. Enjoy the game!"
+
+    def endPlay(self):
+        print "Cricket Game Finished!"
+
+
+class Football(Game):
+    def __init__(self):
+        Game.__init__(self)
+
+    def initialize(self):
+        print "Football Game Initialized! Start playing."
+
+    def startPlay(self):
+        print "Football Game Started. Enjoy the game!"
+
+    def endPlay(self):
+        print "Football Game Finished!"
+# Cricket Football
+
+
+class TemplatePatternDemo:
+    def run(self):
+        game = Cricket()
+        game.play()
+        print '\n'
+        game = Football()
+        game.play()
+T = TemplatePatternDemo()
+T.run()
+
