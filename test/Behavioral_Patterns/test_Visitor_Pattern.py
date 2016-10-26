ca905e8486f0f271f68c4bde096a9af0c532b34e
IvanaXu <1440420407@qq.com>
2016-10-26 11:53:47 +0800

    Create test_Visitor_Pattern.py

---
+++ test/Behavioral_Patterns/test_Visitor_Pattern.py
@@ -0,0 +1,89 @@
+# -*-coding:utf-8-*-
+# @auth ivan
+# @time 2016-10-26 11:21:30
+# @goal test for Visitor Pattern
+
+
+class ComputerPart:
+    def __init__(self):
+        return
+
+    def accept(self, computerPartVisitor):
+        return
+
+
+class Keyboard(ComputerPart):
+    def __init__(self):
+        ComputerPart.__init__(self)
+        return
+
+    def accept(self, computerPartVisitor):
+        computerPartVisitor.visit(self)
+
+
+class Monitor(ComputerPart):
+    def __init__(self):
+        ComputerPart.__init__(self)
+        return
+
+    def accept(self, computerPartVisitor):
+        computerPartVisitor.visit(self)
+
+
+class Mouse(ComputerPart):
+    def __init__(self):
+        ComputerPart.__init__(self)
+        return
+
+    def accept(self, computerPartVisitor):
+        computerPartVisitor.visit(self)
+# Keyboard Monitor Mouse
+
+
+class Computer(ComputerPart):
+    def __init__(self):
+        ComputerPart.__init__(self)
+        self.parts = [Keyboard(), Monitor(), Mouse()]
+        return
+
+    def accept(self, computerPartVisitor):
+        for i in self.parts:
+            i.accept(computerPartVisitor)
+        computerPartVisitor.visit(self)
+
+
+class ComputerPartVisitor:
+    def __init__(self):
+        return
+
+    def visit(self, part):
+        return
+
+
+class ComputerPartDisplayVisitor(ComputerPartVisitor):
+    def __init__(self):
+        ComputerPartVisitor.__init__(self)
+        return
+
+    def visit(self, part):
+        # N O G O O D
+        if str(part.__class__) == '__main__.Keyboard':
+            print 'Displaying Keyboard.'
+        if str(part.__class__) == '__main__.Monitor':
+            print 'Displaying Monitor.'
+        if str(part.__class__) == '__main__.Mouse':
+            print 'Displaying Mouse.'
+        if str(part.__class__) == '__main__.Computer':
+            print 'Displaying Computer.'
+# C = Mouse()
+# D = ComputerPartDisplayVisitor()
+# D.visit(C)
+
+
+class VisitorPatternDemo:
+    def run(self):
+        self.computer = Computer()
+        self.computer.accept(ComputerPartDisplayVisitor())
+V = VisitorPatternDemo()
+V.run()
+
